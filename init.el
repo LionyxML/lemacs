7d01999b03c4e82fd5a8495acfd5202645ffa3f7
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-01-21 23:01:26 -0300

    fix: Hack fonts call on Linux

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;; Author: Rahul M. Juliato <rahul.juliato@gmail.com>
 ;; URL: https://github.com/LionyxML/lemacs
 ;; Keywords: config, emacs, init
-;; Version: 0.1.12
+;; Version: 0.1.13
 ;; Package-Requires: ((emacs "29"))
 
 ;;; Commentary:
@@ -256,7 +256,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 130 :family "Hack Nerd Font" :embolden true))))
+ '(default ((t (:height 110 :family "Hack" :embolden true))))
  '(term ((t (:background "black" :foreground "gray100")))))
 
 ;;; --------------------------------- GENERAL FACES
@@ -267,7 +267,7 @@
 
 (unless (eq system-type 'darwin)
 (custom-set-faces
- '(default ((t (:height 110 :family "Hack Nerd Font" :embolden true))))
+ '(default ((t (:height 110 :family "Hack" :embolden true))))
   '(term ((t (:background "black" :foreground "gray100"))))))
 
 
