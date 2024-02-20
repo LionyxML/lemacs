8d64725c6366850bc7103ed8abe430652fdaae2a
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-02-20 19:56:06 -0300

    feat: Adds embark which key indicator

--- init.el
+++ init.el
@@ -843,7 +843,45 @@
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
                  nil
-                 (window-parameters (mode-line-format . none)))))
+                 (window-parameters (mode-line-format . none))))
+
+  (defun embark-which-key-indicator ()
+	"An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+	(lambda (&optional keymap targets prefix)
+      (if (null keymap)
+          (which-key--hide-popup-ignore-command)
+		(which-key--show-keymap
+		 (if (eq (plist-get (car targets) :type) 'embark-become)
+			 "Become"
+           (format "Act on %s '%s'%s"
+                   (plist-get (car targets) :type)
+                   (embark--truncate-target (plist-get (car targets) :target))
+                   (if (cdr targets) "…" "")))
+		 (if prefix
+			 (pcase (lookup-key keymap prefix 'accept-default)
+               ((and (pred keymapp) km) km)
+               (_ (key-binding prefix 'accept-default)))
+           keymap)
+		 nil nil t (lambda (binding)
+					 (not (string-suffix-p "-argument" (cdr binding))))))))
+
+  (setq embark-indicators
+		'(embark-which-key-indicator
+		  embark-highlight-indicator
+		  embark-isearch-highlight-indicator))
+
+  (defun embark-hide-which-key-indicator (fn &rest args)
+	"Hide the which-key indicator immediately when using the completing-read prompter."
+	(which-key--hide-popup-ignore-command)
+	(let ((embark-indicators
+           (remq #'embark-which-key-indicator embark-indicators)))
+      (apply fn args)))
+
+  (advice-add #'embark-completing-read-prompter
+              :around #'embark-hide-which-key-indicator))
 
 ;; Consult users will also want the embark-consult package.
 (use-package embark-consult
@@ -856,12 +894,6 @@
 
 (use-package eshell
   :config
-  (defun my-eshell-keybindings ()
-	"Define custom key bindings for eshell mode."
-	(define-key eshell-mode-map (kbd "C-p") 'eshell-previous-input)
-	(define-key eshell-mode-map (kbd "C-n") 'eshell-next-input))
-  (add-hook 'eshell-mode-hook 'my-eshell-keybindings)
-
   (setq eshell-prompt-function
 		(lambda ()
           (concat
