331a05b895729370ce9a7fb19cf32088e0488d90
Rahul M. Juliato <rahul.juliato@gmail.com>
2024-06-05 22:55:12 -0300

    feat: removes the initial flash hack

--- early-init.el
+++ early-init.el
@@ -4,14 +4,6 @@
 
 (setenv "LSP_USE_PLISTS" "true")
 
-(defun lemacs/avoid-initial-flash-of-light ()
-  "Avoid flash of light when starting Emacs."
-    (setq mode-line-format nil)
-    (set-face-attribute 'default nil :background "#191724" :foreground "#ffffff")
-    (set-face-attribute 'mode-line nil :background "#191724" :foreground "#ffffff" :box 'unspecified))
-
-(lemacs/avoid-initial-flash-of-light)
-
 (setq frame-resize-pixelwise t
       frame-inhibit-implied-resize t
       frame-title-format '("Emacs"))
