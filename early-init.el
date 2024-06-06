ffdb3d2825a192c83966a3108457495b194bfad2
Rahul M. Juliato <rahul.juliato@gmail.com>
2024-06-05 23:00:45 -0300

    feat: improved no initial flash of light function

--- early-init.el
+++ early-init.el
@@ -4,6 +4,17 @@
 
 (setenv "LSP_USE_PLISTS" "true")
 
+(defun lemacs/avoid-initial-flash-of-light ()
+  "Avoid flash of light when starting Emacs."
+  (setq mode-line-format nil)
+  ;; These colors should match your selected theme for maximum effect
+  ;; Note that for catppuccin whenever we create a new frame or open it on terminal
+  ;; it is necessary to reload the theme.
+  (set-face-attribute 'default nil :background "#1E1E2D" :foreground "#ffffff")
+  (set-face-attribute 'mode-line nil :background "#1E1E2D" :foreground "#ffffff" :box 'unspecified))
+
+(lemacs/avoid-initial-flash-of-light)
+
 (setq frame-resize-pixelwise t
       frame-inhibit-implied-resize t
       frame-title-format '("Emacs"))
