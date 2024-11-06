2a5324b492854fd7227a941158b23d67584f7ea0
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-11-06 08:00:45 -0300

    feat(org): visual customizations for org-mode

--- init.el
+++ init.el
@@ -914,7 +914,33 @@ If INCLUDE-FILE-NAME is non-nil, include the file name in the tab name."
 (use-package org
   :ensure nil
   :defer t
-  :mode ("\\.org\\'" . org-mode))
+  :mode ("\\.org\\'" . org-mode)
+  :config
+  (setq
+   ;; Edit settings
+   org-auto-align-tags nil
+   org-tags-column 0
+   org-catch-invisible-edits 'show-and-error
+   org-special-ctrl-a/e t
+   org-insert-heading-respect-content t
+
+   ;; Org styling, hide markup etc.
+   org-hide-emphasis-markers t
+   org-pretty-entities t
+
+   ;; Agenda styling
+   org-agenda-tags-column 0
+   org-agenda-block-separator ?─
+   org-agenda-time-grid
+   '((daily today require-timed)
+     (800 1000 1200 1400 1600 1800 2000)
+     " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+   org-agenda-current-time-string
+   "◀── now ─────────────────────────────────────────────────")
+
+  ;; Ellipsis styling
+  (setq org-ellipsis " ▼ ")
+  (set-face-attribute 'org-ellipsis nil :inherit 'default :box nil))
 
 (use-package recentf
   :ensure nil
@@ -1765,6 +1791,12 @@ If INCLUDE-FILE-NAME is non-nil, include the file name in the tab name."
   :bind
   (("C-S-p" . org-ros))
   :config)
+
+(use-package org-modern
+  :defer t
+  :ensure t
+  :hook
+  (org-mode . org-modern-mode))
 
 (use-package olivetti
   :defer t
