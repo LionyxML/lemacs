e0a795badcc48b93336650783d0f9b0e525c9dce
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-11-11 19:40:21 -0300

    feat(installer): add options for debugging

--- lemacs-reinstall.sh
+++ lemacs-reinstall.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 clear
 echo ">>> (re)Installing LEmacs ..."
 sleep 2
@@ -10,5 +9,11 @@ sleep 2
 
 echo ">>> Starting Emacs and auto-package fetching/installing ..."
 sleep 2
-emacs --init-dir="./" -nw --eval="(lemacs/first-install)" --debug-init
+
+# NOTE: for debugging porpouses
+# mkdir elpa/
+# echo "((emacs-lisp-mode . ((no-byte-compile . t))))" > elpa/.dir-locals.el
+#
 # emacs --init-dir="./" -nw --debug-init
+
+emacs --init-dir="./" -nw --eval="(lemacs/first-install)" --debug-init
