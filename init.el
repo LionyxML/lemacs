5957200c54753e43c8d8932b477f05ae3d56fb1b
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-11-11 19:42:25 -0300

    feat(treesit): make auto install not prompt

--- init.el
+++ init.el
@@ -2050,7 +2050,7 @@ If INCLUDE-FILE-NAME is non-nil, include the file name in the tab name."
   :ensure t
   :defer t
   :custom
-  (treesit-auto-install 'always)
+  (treesit-auto-install t)
   :hook
   (after-init . global-treesit-auto-mode)
   :config
